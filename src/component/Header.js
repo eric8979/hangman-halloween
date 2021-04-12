e94cd59fd234ecca9a1e15ec2cf588c21d6c25d2
eric8979 <hsh048148@gmail.com>
2021-04-12 10:57:25 +0900

    initial commit (header, background completed)

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>👻 HangMan 👻</h1>
+      <h2>Find the hidden word!</h2>
+    </div>
+  );
+};
+
+export default Header;

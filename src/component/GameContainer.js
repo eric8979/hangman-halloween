49634a461201025d520eb62d54bc8a5d4cde91c8
eric8979 <hsh048148@gmail.com>
2021-04-12 18:13:25 +0900

    design loading, landing page

--- src/component/GameContainer.js
+++ src/component/GameContainer.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 const GameContainer = () => {
-  return <div className='gameContainer'>Game Container</div>;
+  let page = (
+    <div className='outer'>
+      <div id='container'>
+        <div id='spooky'>
+          <div id='body'>
+            <div id='eyes'></div>
+            <div id='mouth'></div>
+            <div id='feet'>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div id='shadow'></div>
+      </div>
+      <br />
+      <span className='loading'>Loading ...</span>
+    </div>
+  );
+
+  let gamePage = <Fragment></Fragment>;
+
+  const [load, doneLoad] = useState(page);
+
+  useEffect(() => {
+    setTimeout(() => {
+      doneLoad(<div className='gameContainer'>done loading</div>);
+    }, 1000);
+  }, []);
+
+  return <Fragment>{load}</Fragment>;
 };
 
 export default GameContainer;
--- src/component/Landing.js
+++ src/component/Landing.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className='landing'>
+    <Fragment>
+      <div className='landing'></div>
+      <div className='lightning flashit'></div>
+
       <div className='header'>
         <h1>🎃 HangMan 🎃</h1>
         <h2>- halloween edition -</h2>
+      </div>
+
+      <div className='bat-container pin-bottom'>
+        <div className='bat-wrapper'>
+          <div className='bat js-bat'>
+            <div className='bat-leg-1'></div>
+            <div className='bat-leg-2'></div>
+          </div>
+        </div>
       </div>
 
       <div className='start-button'>
@@ -14,7 +26,7 @@ const Header = () => {
           Start!
         </Link>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
